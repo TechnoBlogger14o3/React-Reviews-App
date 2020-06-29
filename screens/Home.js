ef5728705954a30594b609b3badcad3950a2d695
Aman Shekhar <Aman Shekhar>
2020-06-29 14:30:45 +0530

    custom-card-components

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/Global';
+import Card from '../shared/Card';
 
 export default function Home({ navigation }) {
 
@@ -17,7 +18,9 @@ export default function Home({ navigation }) {
         data = {reviews}
         renderItem={({ item }) => (
           <TouchableOpacity onPress = {() => navigation.navigate('Reviews', item)}>
-            <Text style = {globalStyles.titleText}>{item.title}</Text>
+            <Card>
+              <Text style = {globalStyles.titleText}>{item.title}</Text>
+            </Card>
           </TouchableOpacity>
           
         )}
--- screens/Reviews.js
+++ screens/Reviews.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { globalStyles } from '../styles/Global';
+import Card from '../shared/Card';
 
 export default function Reviews({navigation}) {
 
   return (
     <View style={globalStyles.container}>
-      <Text>{navigation.getParam('title')}</Text>
-      <Text>{navigation.getParam('body')}</Text>
-      <Text>Rating is {navigation.getParam('rating')}</Text>
+      <Card>
+        <Text>{navigation.getParam('title')}</Text>
+        <Text>{navigation.getParam('body')}</Text>
+        <Text>Rating is {navigation.getParam('rating')}</Text>
+      </Card>
     </View>
   );
 }
+
+
+
+
