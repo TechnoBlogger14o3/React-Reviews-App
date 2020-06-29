12786c0cc8e2a59dc8e6cfc4effa9805157e8ad7
Aman Shekhar <Aman Shekhar>
2020-06-29 15:01:15 +0530

    using images, and background images

--- shared/Header.js
+++ shared/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ navigation, title }) {
@@ -9,16 +9,17 @@ export default function Header({ navigation, title }) {
     }
 
     return(
-        <View style = {styles.header}> 
+        <ImageBackground source = {require('../assets/game_bg.png')} style = {styles.header}> 
             {/** Icon for Menu */}
             <MaterialIcons name = 'menu' size ={28} onPress={openMenu} style = {styles.icon} />
-            <View>
+            <View style = {styles.headerTitle}>
+                <Image source = {require('../assets/heart_logo.png')} style = {styles.headerImage}></Image>
                 <Text style = {styles.headerText}>
                     {title}
                 </Text>
             </View>
 
-        </View>
+        </ImageBackground>
     )
 }
 
@@ -39,5 +40,13 @@ const styles = StyleSheet.create({
     icon: {
         position: 'absolute',
         left: 16
+    }, 
+    headerImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10
+    }, 
+    headerTitle: {
+        flexDirection: 'row',
     }
 })
